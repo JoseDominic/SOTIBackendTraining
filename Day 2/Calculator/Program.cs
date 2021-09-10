d77150376e927cc4599b15a5699252976b7658e6
JoseDominic <josudominic007@gmail.com>
2021-09-10 14:25:51 +0530

    afternoon

--- Day 2/Calculator/Program.cs
+++ Day 2/Calculator/Program.cs
@@ -11,9 +11,16 @@ namespace Calculator
         {
             return a + b;
         }
+
         static void Main(string[] args)
         {
-            int x, y, sum;
+            int x, y, sum, result;
+            //type conversion examples
+            string val1 = "21", val2 = "100.11";
+            bool z = int.TryParse(val1,out result); //Try parse makes out variable false if conversion fails
+            Console.WriteLine(result);
+            Console.WriteLine(int.TryParse(val2,out result));
+            Console.WriteLine(val2);
             Console.WriteLine("Enter first num:");
             x = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter second num:");
@@ -21,6 +28,7 @@ namespace Calculator
             Console.WriteLine($"x={x} y={y}");
             sum = add(x, y);
             Console.WriteLine($"Sum = {sum}");
+            Console.ReadLine();
         }
     }
 }
