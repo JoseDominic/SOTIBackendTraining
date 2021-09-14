b67cb3a85247943a8a12aa5ba7319a0e618f34b0
JoseDominic <josudominic007@gmail.com>
2021-09-14 17:07:13 +0530

    day 4 afternoon

--- Day 4/HMS-demo/Program.cs
+++ Day 4/HMS-demo/Program.cs
@@ -6,6 +6,26 @@ using System.Threading.Tasks;
 
 namespace HMS_demo
 {
+    interface iUser
+    {
+        void work();
+    }
+    interface A : iUser
+    {
+        //extending interface example
+    }
+    class User:iUser
+    {
+        public int a
+        {
+            get => a;
+            set => a = value;
+        }
+        public void work()
+        {
+            Console.WriteLine("Works");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
